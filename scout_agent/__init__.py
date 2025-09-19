b225b3b7b9de5f233f2c981159705ebe46816bec
George <george@ishaq.com>
2025-09-19 14:35:39 -0700

    Fixed imports I think

--- scout_agent/__init__.py
+++ scout_agent/__init__.py
@@ -5,7 +5,7 @@ The Scout Agent is responsible for gathering threat intelligence and building a 
 by scraping vulnerability feeds, CVE databases, and security patches from the web.
 """
 
-from .scout_agent import ScoutAgent
+from .scout_agent import ScoutAgent, VulnerabilityData, PatchData
 from .mcp_stdio_client import BrightDataMCPStdioClient
 
-__all__ = ['ScoutAgent', 'BrightDataMCPStdioClient']
+__all__ = ['ScoutAgent', 'VulnerabilityData', 'PatchData', 'BrightDataMCPStdioClient']
